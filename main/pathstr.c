629da51a6ed109e1078e328cca873a1740d02775
Pie Pie <i.jalal@aui.ma>
2023-05-18 09:39:54 +0100

    Create pathstr.c

---
+++ main/pathstr.c
@@ -0,0 +1,31 @@
+#include<main.h>
+
+/**
+ * pathstr - function that prints the path string
+ * @right: string after "PATH ="
+ * @first: first tokenized word
+ * Return: 0 for success
+ */
+char *pathstr(char *right, char *first)
+{
+	char *new = NULL;
+	char *token = NULL;
+	int token_len = 0, first_len = 0;
+
+	token = right;
+	token_len = _strlen(token);
+	first_len = _strlen(first);
+
+	new = malloc((token_len + first_len + 2) * sizeof(char));
+	if (new == NULL)
+		return (NULL);
+
+	new[0] = '\0';
+
+	_strcat(new, right);
+	_strcat(new, "/");
+	_strcat(new, first);
+	_strcat(new, "\0");
+
+	return (new);
+}
