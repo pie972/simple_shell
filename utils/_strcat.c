f4d9f472e20cbc0f70215e140645b6b2aac51256
Pie Pie <i.jalal@aui.ma>
2023-05-18 09:56:35 +0100

    Create _strcat.c

---
+++ utils/_strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcat - concatenate 2 strings
+ *@dest: is a character
+ *@src: is a character
+ *Return: dest concatenated strind
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{ }
+
+	for (j = 0; src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+
+	dest[i] = '\0';
+	return (dest);
+}
