2c00d91720cd316cdd555b28f942404f33cef68e
fletcherayoub <douch202@gmail.com>
2023-05-20 02:30:31 -0700

    again

--- _putchar.c
+++ _putchar.c
@@ -2,10 +2,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: the cheracter to print
  *
- * Return: on success 1
- * on error, -1 is returned, and errno is set appropriately
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
