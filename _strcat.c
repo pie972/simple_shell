82d03fa7ae41963e3b2b68be00cef7909ab9fb2f
fletcherayoub <douch202@gmail.com>
2023-05-22 03:58:18 -0700

    update again

--- _strcat.c
+++ _strcat.c
@@ -9,14 +9,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int n, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (n = 0; dest[n] != '\0'; n++)
 	{ }
 
-	for (j = 0; src[j] != '\0'; j++, i++)
-		dest[i] = src[j];
+	for (j = 0; src[j] != '\0'; j++, n++)
+		dest[n] = src[j];
 
-	dest[i] = '\0';
+	dest[n] = '\0';
 	return (dest);
 }
