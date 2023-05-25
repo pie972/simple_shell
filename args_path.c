3ae817b19a8139bcb80e36548e98d2c759ad9bf6
fletcherayoub <douch202@gmail.com>
2023-05-25 04:23:38 -0700

    update

--- args_path.c
+++ args_path.c
@@ -15,7 +15,7 @@ char *args_path(char **parse, char **new)
 
 	for (i = 0; new[i]; i++)
 	{
-		total = (char *)malloc(60);
+		total = malloc(60);
 		_strcat(total, new[i]);
 		_strcat(total, "/");
 		_strcat(total, parse[0]);
@@ -24,7 +24,7 @@ char *args_path(char **parse, char **new)
 		{
 			for (k = 0; parse[k] != NULL && *parse[k] != '\0'; k++)
 				;
-			cat = (char **)malloc(sizeof(char *) * (k + 1));
+			cat = malloc(sizeof(char *) * (k + 1));
 			cat[k] = NULL;
 			cat[0] = _strdup(total);
 
